235ec673131615bfb474df28a0873cc57ebc3e18
Cheewai Lai <cheewai.lai@gmail.com>
2016-11-23 09:37:47 +0200

    Fixed docker-entrypoint.sh for Debian

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
+RUNUSER_UID="${RUNUSER_UID:-1000}"
+RUNUSER_HOME="${RUNUSER_HOME:-/home/runuser}"
 set -ux
-adduser -u $RUNUSER_UID -h /home/runuser -D -s /bin/bash runuser
+if [ -x /usr/sbin/useradd ]; then
+  useradd -s /bin/false --no-create-home --home-dir "$RUNUSER_HOME" -u $RUNUSER_UID runuser
+else
+  adduser -s /bin/false -D -h $RUNUSER_HOME -H -u $RUNUSER_UID runuser
+fi
 exec gosu runuser "$@"
